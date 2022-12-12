116d5d162a30a6cf5569a2b9cd6008bd354e0172
diego1005 <dmaguilar82@gmail.com>
2022-12-12 16:51:44 -0300

    MAKE: show sidebar options when login

--- src/Components/Main/Sidebar/SidebarMenu/SidebarMenu.jsx
+++ src/Components/Main/Sidebar/SidebarMenu/SidebarMenu.jsx
@@ -1,18 +1,27 @@
+import { useContext } from 'react';
 import './SidebarMenu.css'
 import SidebarOpt from './SidebarOpt/SidebarOpt';
 import { sidebarOptions } from '../../../../Common/Sidebar/SidebarOptions';
+import { AppContext } from '../../../../Context/context';
 
 export default function SidebarMenu() {
+
+    const { userLogged } = useContext(AppContext);
+
     return (
         <div className='sidebar-menu'>
             {
-                sidebarOptions.map(({ icon, option }, idx) =>
+                userLogged
+                ? sidebarOptions.map(({ icon, option }, idx) =>
                     <SidebarOpt
                         key={idx}
                         icon={icon}
                         opt={option}
                     />
                 )
+                : <i className="fa-solid fa-right-to-bracket">
+                    <em> Por favor inicia sesion</em>
+                </i>
             }
         </div>
     )
