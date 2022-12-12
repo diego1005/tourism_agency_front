a77e2f62a5ae01c12bed95a69dd5c16c462b9982
diego1005 <dmaguilar82@gmail.com>
2022-12-12 14:32:40 -0300

    working on sidebar

--- src/Components/Main/Sidebar/SidebarMenu/SidebarOpt/SidebarOpt.jsx
+++ src/Components/Main/Sidebar/SidebarMenu/SidebarOpt/SidebarOpt.jsx
@@ -6,13 +6,15 @@ export default function SidebarOpt({ icon, opt }) {
     const { handleView } = useHandleView();
 
     return (
-        <button className='sidebar-opt-container' onClick={e => handleView(e, opt)}>
-            <div className="sidebar-opt">
-                {opt}
-            </div>
-            <div className="sidebar-opt-icon">
-                {icon}
-            </div>
-        </button>
+        <div className='sidebar-opt-container'>
+            <button className='sidebar-opt-btn' onClick={e => handleView(e, opt)}>
+                <div className="sidebar-opt">
+                    {opt}
+                </div>
+                <div className="sidebar-opt-icon">
+                    {icon}
+                </div>
+            </button>
+        </div>
     )
 }
